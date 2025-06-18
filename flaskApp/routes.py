808fd9225a4a840819f7e1a9c89bd06435a6d797
May <mayrozen45@gmail.com>
2025-06-18 11:10:19 +0300

    trying routs

--- flaskApp/routes.py
+++ flaskApp/routes.py
@@ -62,7 +62,10 @@ def run_algorithm():
             valuations[p][g] = v
 
     # build and run
-    agent_caps = {p: num_items for p in players}
+    agent_caps = {
+        p: sum(1 for v in valuations[p].values() if v > 0)
+        for p in players
+    }
     item_caps = {g: 1 for g in items}
 
     instance   = Instance(
